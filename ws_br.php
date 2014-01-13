ddec5988ebacdb8925b9befe9ba30ec8e57129db
Herve Senot <herve.senot@groundtruth.com.au>
2014-01-13 18:36:04 +1100

    Modified for bicycle routing

--- ws_br.php
+++ ws_br.php
@@ -4,13 +4,13 @@
 
 	// G for Google API, M for MapQuest
 	$mode="M";
-	$filename="b3";
+	$filename="B3";
 
 	echo "Starting process: reading records from ".$filename.".csv and using ".$mode." API ...";
 	flush();
 
-	$fr = fopen("in/".$filename.".csv", "r");
-	$fw = fopen("out/".$filename.".csv", 'w');
+	$fr = fopen("./in/".$filename.".csv", "r");
+	$fw = fopen("./out/".$filename.".csv", 'w');
 
 	while (!feof($fr) ) {
 		$line_of_text = fgetcsv($fr, 1024, ';');
@@ -29,9 +29,9 @@
 			// http://open.mapquestapi.com/directions/#advancedparameters
 			// Note from the MapQuest Directions API page: http://developer.mapquest.com/web/products/open/directions-service 
 			// If your application will get heavy usage, please let us know by sending us an email at open@mapquest.com. Please include the estimate of your expected usage so that we will be aware and accommodate the extra traffic.
-			$url = "http://open.mapquestapi.com/directions/v1/route?outFormat=json&from=".$line_of_text[2].",".$line_of_text[1]."&to=".$line_of_text[4].",".$line_of_text[3]."&shapeFormat=cmp&generalize=0";
+			$url = "http://open.mapquestapi.com/directions/v1/route?outFormat=json&from=".$line_of_text[2].",".$line_of_text[1]."&to=".$line_of_text[4].",".$line_of_text[3]."&shapeFormat=cmp&generalize=0&routeType=bicycle&key=Fmjtd|luuanuuyll%2C7s%3Do5-96bx9r";
 		}
-		//echo $url;
+		echo $url."\n";
 
 		$contents = file_get_contents($url);
 
@@ -39,11 +39,11 @@
 
 		$line_out = array($line_of_text[0]);
 		$route_steps='';
+		$route_steps_counter=0;
 
 		if ($mode=="G")
 		{
 			$route_steps_length=count($json["routes"][0]["legs"][0]["steps"]);
-			$route_steps_counter=0;
 		
 			foreach ($json["routes"][0]["legs"][0]["steps"] as $r)
 			{
@@ -62,6 +62,7 @@
 			// The list of long/lat for the whole route is encoded in json.route.shape.shapePoints
 			foreach(decodePolylineToArray($json["route"]["shape"]["shapePoints"]) as $c)
 			{
+				$route_steps_counter++;
 				$route_steps .= $c[1]." ".$c[0].",";
 			}
 		}
